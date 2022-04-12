c04dd25b2de3e61e313abd87f241f60d390593e4
mafei <zhdn_791005@163.com>
2022-04-12 15:34:50 +0800

    optimize login and main page

--- static/assets/js/logon.js
+++ static/assets/js/logon.js
@@ -297,20 +297,20 @@ var trans = {
   Build.prototype.run = function() {
     this.anim();
 
-    window.addEventListener('mousemove', function(e) {
-      this.toX = (e.clientX - this.canvas.width / 2) * -0.8;
-      this.toY = (e.clientY - this.canvas.height / 2) * 0.8;
-    }.bind(this));
+    // window.addEventListener('mousemove', function(e) {
+    //   this.toX = (e.clientX - this.canvas.width / 2) * -0.8;
+    //   this.toY = (e.clientY - this.canvas.height / 2) * 0.8;
+    // }.bind(this));
     window.addEventListener('touchmove', function(e) {
       e.preventDefault();
       this.toX = (e.touches[0].clientX - this.canvas.width / 2) * -0.8;
       this.toY = (e.touches[0].clientY - this.canvas.height / 2) * 0.8;
     }.bind(this));
-    window.addEventListener('mousedown', function(e) {
-      for (var i = 0; i < 100; i++) {
-        this.add();
-      }
-    }.bind(this));
+    // window.addEventListener('mousedown', function(e) {
+    //   for (var i = 0; i < 100; i++) {
+    //     this.add();
+    //   }
+    // }.bind(this));
     window.addEventListener('touchstart', function(e) {
       e.preventDefault();
       for (var i = 0; i < 100; i++) {
